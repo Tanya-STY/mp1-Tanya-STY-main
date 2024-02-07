6a49a66b2f35b8493cd5b23d26cb076879722342
Tanya So Tin Yan <tanya.sty99@gmail.com>
2024-02-07 14:41:28 -0500

    unversioned file

---
+++ app/src/test/java/com/bradteachescode/basiccalculator/ExampleUnitTest.java
@@ -0,0 +1,24 @@
+package com.bradteachescode.basiccalculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+
+    /* Initial test for TDD */
+    @Test
+    public void testAddDecimalPoint() {
+        assertEquals("Incorrect result",4.6, 2.4 + 2.2, 0.01);
+    }
+}
+
